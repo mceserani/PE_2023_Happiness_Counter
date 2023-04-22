eec58992dfc8a17acfe4a22524cac60a1194a7ac
Matteo Ceserani <ceserani@gmail.com>
2023-04-22 17:31:44 +0200

    happiness.py added

---
+++ happiness.py
@@ -0,0 +1,10 @@
+"""Calcolatore di felicità"""
+
+n, m = map(int,input().split())
+
+# SCRIVI QUI IL TUO CODICE
+# 1. Acquisisci la lista e gli insiemi A e B
+#    Usa list e set comprehension
+# 2. Calcola la felicità
+#    Usa un ciclo for
+# 3. Stampa la felicità
