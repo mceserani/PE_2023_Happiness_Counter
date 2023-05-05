0960361c9736fbe8e227678c0723cf9fb4dd5284
Matteo Ceserani <ceserani@gmail.com>
2023-05-05 18:40:46 +0200

    Minor changes

--- happiness.py
+++ happiness.py
@@ -2,9 +2,18 @@
 
 n, m = map(int,input().split())
 
-# SCRIVI QUI IL TUO CODICE
-# 1. Acquisisci la lista e gli insiemi A e B
-#    Usa list e set comprehension
-# 2. Calcola la felicità
-#    Usa un ciclo for
-# 3. Stampa la felicità
+L = list(map(int,input().split()))
+A = set(map(int,input().split()))
+B = set(map(int,input().split()))
+
+H = 0
+for i in L:
+    if i in A:
+        H += 1
+    elif i in B:
+        H -= 1
+
+print(H)
+
+# test_case expected output: 15
+# test_case_long expected output: -100
